69e1bb0dd40d3e9de9225f20b6f7858cccd38a05
Amir Ismael Mandanas <amirismaelmandanas@Amirs-MacBook-Air.local>
2026-03-02 21:02:39 +0800

    created an accounts app

--- ledger/views.py
+++ ledger/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Recipe, Ingredient, RecipeIngredient
 
-
 def recipe_list(request):
     recipes = Recipe.objects.all()
     dictionary = {"recipes" : recipes}
     return render(request, "ledger/recipe_list.html", dictionary)
+
 
 def recipe_detail(request, pk):
     recipe = Recipe.objects.get(pk=pk)
