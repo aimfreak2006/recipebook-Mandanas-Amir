9786e45992c05928fcb5955dfc5e55a290a69b27
Amir Ismael Mandanas <amirismaelmandanas@Amirs-MacBook-Air.local>
2026-03-02 23:27:56 +0800

    Added password reset flow, user can now change passwords

--- ledger/views.py
+++ ledger/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Recipe, Ingredient, RecipeIngredient
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def recipe_list(request):
     recipes = Recipe.objects.all()
     dictionary = {"recipes" : recipes}
     return render(request, "ledger/recipe_list.html", dictionary)
 
-
+@login_required
 def recipe_detail(request, pk):
     recipe = Recipe.objects.get(pk=pk)
     dictionary = {"recipe" : recipe}
