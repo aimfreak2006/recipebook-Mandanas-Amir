57082baa365d16fea08df5e9fcb97a3b1c144246
Amir Ismael Mandanas <amirismaelmandanas@Amirs-MacBook-Air.local>
2026-03-02 21:57:53 +0800

    Updated urls in project level and added templates under project

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import recipe_list, recipe_detail
 
 urlpatterns = [
-    path('', recipe_list, name='recipe_list'),
+    path('recipe/', recipe_list, name='recipe_list'),
     path('recipe/<int:pk>', recipe_detail, name='recipe_detail'),
 ] 
 
--- recipebook/urls.py
+++ recipebook/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('ledger.urls', namespace='ledger')),
+    path('ledger/', include('ledger.urls', namespace='ledger')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
