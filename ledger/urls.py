d116d76b593abcfa89525d44d5ae59f8c31c22fa
Amir Ismael Mandanas <amirismaelmandanas@Amirs-MacBook-Air.local>
2026-03-12 14:44:56 +0800

    added a form page that successfully adds the recipe in recipes/list page

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,7 +3,7 @@ from .views import recipe_list, recipe_detail, recipe_add
 
 urlpatterns = [
     path('recipes/list', recipe_list, name='recipe_list'),
-    path('recipe/add', recipe_add, name='recipe_add')
+    path('recipe/add', recipe_add, name="recipe_add"),
     path('recipe/<int:pk>', recipe_detail, name='recipe_detail'),
 ] 
 
--- ledger/views.py
+++ ledger/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Recipe, Ingredient, RecipeIngredient
+from accounts.models import Profile
 from django.contrib.auth.decorators import login_required
 
 
@@ -10,21 +11,17 @@ def recipe_list(request):
     if (request.method == "POST"):
         recipe = Recipe()
         recipe.name = request.POST.get("recipe_name")
-        recipe.author = request.POST.get("recipe_author")
+        author_primary_key = int(request.POST.get("recipe_author"))
+        recipe.author = Profile.objects.get(pk=author_primary_key)
         recipe.created_on = request.POST.get("recipe_created")
         recipe.updated_on = request.POST.get("recipe_updated")
         recipe.save()
     return render(request, "ledger/recipe_list.html", dictionary)
 
-@login_required
 def recipe_add(request):
-    if (request.method == "POST"):
-        recipe = Recipe()
-        recipe.name = request.POST.get("recipe_name")
-        recipe.author = request.POST.get("recipe_author")
-        recipe.created_on = request.POST.get("recipe_created")
-        recipe.updated_on = request.POST.get("recipe_updated")
-        recipe.save()
+    authors = Profile.objects.all()
+    dictionary = {"authors" : authors}
+    return render(request, "ledger/recipe_add.html", dictionary)
 
 @login_required
 def recipe_detail(request, pk):
